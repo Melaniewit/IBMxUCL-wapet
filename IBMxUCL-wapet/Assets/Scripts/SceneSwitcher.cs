fbf4f3cf5c04fe88d2ae81b051efc1d3564a9220
Melanie <ruohongwei.wrh@gmail.com>
2024-07-16 00:31:44 +0100

    scene switching developing

--- IBMxUCL-wapet/Assets/Scripts/SceneSwitcher.cs
+++ IBMxUCL-wapet/Assets/Scripts/SceneSwitcher.cs
@@ -6,12 +6,12 @@ public class SceneSwitcher : MonoBehaviour
     // Method to load the settings scene
     public void LoadSettingsScene()
     {
-        SceneManager.LoadScene("Setting page"); // Replace "SettingsScene" with the actual name of your settings scene
+        SceneManager.LoadScene("Setting page");
     }
 
     // Method to load the login scene
     public void LoadLoginScene()
     {
-        SceneManager.LoadScene("Login page"); // Replace "LoginScene" with the actual name of your login scene
+        SceneManager.LoadScene("Login page");
     }
 }
